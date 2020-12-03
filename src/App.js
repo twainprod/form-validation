0225c844c5a1e72b3a8ac2e88e15d26610a3ea85
Vlad Kisel <ipon98@gmail.com>
2020-12-03 02:01:10 +0200

    Update design with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {  
   const [number, setNumber] = useState('');
@@ -37,84 +37,98 @@ const App = () => {
   }
 
   const numberHandler = (e) => {
-    var foo = e.target.value.split("-").join("");
-    if (foo.length > 0) {
-        foo = foo.match(new RegExp('.{1,4}', 'g')).join("-");
-    }
-    const value = foo;
-    setNumber(value);
-    const re = /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/;
-    if (!re.test(String(e.target.value))) {
-      setNumberError('Некорректный номер карты')
-      if (!e.target.value) {
-        setNumberError('Поле не может быть пустым')
+      var foo = e.target.value.split("-").join("");
+      if (foo.length > 0) {
+          foo = foo.match(new RegExp('.{1,4}', 'g')).join("-");
       }
-    } else {
-      setNumberError('')
-    }
+      const value = foo;
+      setNumber(value);
+      const re = /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/;
+      if (!re.test(String(e.target.value))) {
+        setNumberError('Некорректный номер карты')
+        if (!e.target.value) {
+          setNumberError('Поле не может быть пустым')
+        }
+      } else {
+        setNumberError('')
+      }
   }
 
   const dateHandler = (e) => {
-  var foo = e.target.value.split("/").join("");
-    if (foo.length > 0) {
-        foo = foo.match(new RegExp('.{1,2}', 'g')).join("/");
-    }
-    const value = foo;
-  setDate(value);
-  const re = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;  
-  if (!re.test(String(e.target.value))) {
-    setDateError('Внимание! Введите дату в формате ММ/ГГ')
-    if (!e.target.value) {
-      setDateError('Поле не может быть пустым')
-    }
-  } else {
-    setDateError('');
-  }
+      var foo = e.target.value.split("/").join("");
+        if (foo.length > 0) {
+            foo = foo.match(new RegExp('.{1,2}', 'g')).join("/");
+        }
+        const value = foo;
+      setDate(value);
+      const re = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;  
+      if (!re.test(String(e.target.value))) {
+        setDateError('Внимание! Введите дату в формате ММ/ГГ')
+        if (!e.target.value) {
+          setDateError('Поле не может быть пустым')
+        }
+      } else {
+        setDateError('');
+      }
   }
 
 const cvvHandler = (e) => {
-  setCvv(e.target.value);
-  console.log(e.target.value);
-  const re = /[0-9]{3}$/;
-  if (!re.test(String(e.target.value))) 
-  {
-    setCvvError('CVV должен содержать только цифры')
-    if (e.target.value < 3) {
-      setCvvError('Длина CVV не может быть короче 3 символов')
-    }
-    if (!e.target.value) {
-      setCvvError('Поле не может быть пустым')
-    }
-  } else {
-    setCvvError('');
-  }
+      setCvv(e.target.value);
+      console.log(e.target.value);
+      const re = /[0-9]{3}$/;
+      if (!re.test(String(e.target.value))) 
+      {
+        setCvvError('CVV должен содержать только цифры')
+        if (e.target.value < 3) {
+          setCvvError('Длина CVV не может быть короче 3 символов')
+        }
+        if (!e.target.value) {
+          setCvvError('Поле не может быть пустым')
+        }
+      } else {
+        setCvvError('');
+      }
 }
   const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('Card number: ' + number +
-      '\nDate: ' + date +
-      '\nCVV2: '+ cvv    
-    )
+      e.preventDefault();
+      alert('Card number: ' + number +
+        '\nDate: ' + date +
+        '\nCVV2: '+ cvv    
+      )
+  }
+
+  const clearAll = () => {  
+      setNumber('');
+      setDate('');
+      setCvv('');      
+      setNumberError('Поле не может быть пустым');
+      setDateError('Поле не может быть пустым')
+      setCvvError('Поле не может быть пустым')      
   }
 
   return (
-    <div className="App">
-      <form onSubmit={e => handleSubmit(e)}>
-            <div className='title'>Form Validation</div>
-            <div className='inputGroup'>          
-                  <div className='inputArea'><div>Номер карты:</div>
-                  <input maxLength='19' name='number' onChange={e => numberHandler(e) } value={number} onBlur={e => blurHandler(e)} type='text' placeholder='XXXX-XXXX-XXXX-XXXX' /></div>
-                  {(touchedNumber && numberError) && <div className='error'>{numberError}</div>}
+    <div className="container d-flex justify-content-center">
+      <form className='col-lg-8' onSubmit={e => handleSubmit(e)}>
+            <div className='display-4 my-3 d-flex justify-content-center'>Form Validation</div>                  
+                <div className='form-group row'>
+                    <div className='col-form-label col-lg-3 col-md-3 col-sm-4'>Номер карты:</div>
+                          <input className='form-control col-lg-9 col-md-9 col-sm-8' maxLength='19' name='number' onChange={e => numberHandler(e) } value={number} onBlur={e => blurHandler(e)} type='text' placeholder='XXXX-XXXX-XXXX-XXXX' /></div>
+                          {(touchedNumber && numberError) && <div role='alert' className='alert alert-danger d-flex align-items-center'><i class="fas fa-exclamation-circle mr-2"></i> {numberError}</div>}
+                          
+                <div className='form-group row'>
+                  <div className='col-form-label col-lg-3 col-md-3 col-sm-4'>Срок действия:</div>
+                        <input className='form-control col-lg-9 col-md-9 col-sm-8' maxLength='5' name='date' onChange={e => dateHandler(e) }value={date} onBlur={e => blurHandler(e)} type='text' placeholder='ММ/ГГ' /></div>
+                        {(touchedDate && dateError) && <div className='alert alert-danger d-flex align-items-center'><i class="fas fa-exclamation-circle mr-2"></i> {dateError}</div>}
                   
-                  <div className='inputArea'><div>Срок действия:</div>
-                  <input maxLength='5' name='date' onChange={e => dateHandler(e) }value={date} onBlur={e => blurHandler(e)} type='text' placeholder='ММ/ГГ' /></div>
-                  {(touchedDate && dateError) && <div className='error'>{dateError}</div>}
-                  
-                  <div className='inputArea'><div>CVV2:</div>
-                  <input maxLength='3' name='cvv' onChange={e => cvvHandler(e) } value={cvv} onBlur={e => blurHandler(e)} type='password' placeholder='CVV2' /></div>
-                  {(touchedCvv && cvvError) && <div className='error'>{cvvError}</div>}
-            </div>
-            <div className='btnBlock'><button disabled={!formValid} type='submit'>Submit</button></div>
+                <div className='form-group row'>
+                  <div className='col-form-label col-lg-3 col-md-3 col-sm-4'>CVV2:</div>
+                        <input className='form-control col-lg-9 col-md-9 col-sm-8' maxLength='3' name='cvv' onChange={e => cvvHandler(e) } value={cvv} onBlur={e => blurHandler(e)} type='password' placeholder='CVV2' /></div>
+                        {(touchedCvv && cvvError) && <div className='alert alert-danger d-flex align-items-center'><i class="fas fa-exclamation-circle mr-2"></i> {cvvError}</div>}
+            
+        <div className='d-flex justify-content-center mt-3'>
+          <button className='btn btn-primary mr-2' disabled={!formValid} type='submit'>Отправить</button>
+          <button className='btn btn-danger' onClick={clearAll}
+           type='reset'>Очистить</button></div>
       </form>
     </div>
   );
